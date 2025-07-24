553e578892503b29f9f47dae0bbf03d0c7f2ada7
ram pravesh <rampraveshsingh1996@gmail.com>
2025-07-24 15:20:18 +0530

    test

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
 import { createUserOrder, inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
-// Create an API that serves zero functions
+// Create an API that serves zero fun
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
